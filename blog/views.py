07338edb0b9cd75c13760862c88acb35dd008d36
klebercperes <kleber@peres.systems>
2025-02-23 05:51:43 +0000

    Setting aup the Urls and start views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+from blog.models import Post    
+from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
+
+def starting_page(request):
+    pass
+
+
+def posts(request):
+    pass
+
+def post_details(request):
+    pass
+
--- my_site/urls.py
+++ my_site/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include('blog.urls')), #blog is the folder of the app and urls is the file the holds the urls.
+    #I'll leave the empty path now because I want to keep the blog app as the main app for now.
+    #But in the future, I might want to add more apps to this project.
+
 ]
