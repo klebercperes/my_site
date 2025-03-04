ee79c4ad2a91acbc56fda3979946169ed8d6c839
klebercperes <kleber@peres.systems>
2025-03-04 09:05:23 +0000

    all-posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post # Import your Post model
 
 
 # Create your views here.
@@ -6,10 +7,10 @@ from django.shortcuts import render
 def starting_page(request):
     return render(request, "blog/index.html")
 
-
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    posts = Post.objects.all() #Fetch all posts from the database
+    return render(request, "blog/all-posts.html", {"posts":posts}) #Pass posts to template.
 
-def post_details(request):
+def post_detail(request):
     return render(request, "blog/post-detail.html")
 
